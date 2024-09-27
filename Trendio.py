d5a8f88a9c26aa9bcd1ba0c57cd3c362c166d681
jeroldleung <jeroldleung@gmail.com>
2024-09-27 15:40:11 +0800

    product prototype

---
+++ Trendio.py
@@ -0,0 +1,24 @@
+import os
+
+import streamlit as st
+from openai import OpenAI
+
+with st.sidebar:
+    st.header("Chat History")
+
+st.title("Trendio")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"], avatar=msg["avatar"]).write(msg["content"])
+
+if prompt := st.chat_input("Say something"):
+    url = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+    client = OpenAI(api_key=os.getenv("DASHSCOPE_API_KEY"), base_url=url)
+    st.session_state.messages.append({"role": "user", "avatar": ":material/face:", "content": prompt})
+    st.chat_message("user", avatar=":material/face:").write(prompt)
+    response = client.chat.completions.create(model="qwen-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "avatar": ":material/smart_toy:", "content": msg})
+    st.chat_message("assistant", avatar=":material/smart_toy:").write(msg)
