d0c8907381fa3f49c8483017329bfc2695133f58
jeroldleung <jeroldleung@gmail.com>
2024-09-27 23:58:08 +0800

    change line length of format style to 80

--- Trendio.py
+++ Trendio.py
@@ -17,7 +17,19 @@ if prompt := st.chat_input("Say something"):
     url = "https://dashscope.aliyuncs.com/compatible-mode/v1"
     client = OpenAI(api_key=os.getenv("DASHSCOPE_API_KEY"), base_url=url)
     st.chat_message("user", avatar=":material/face:").write(prompt)
-    st.session_state.messages.append({"role": "user", "avatar": ":material/face:", "content": prompt})
-    stream = client.chat.completions.create(model="qwen-turbo", messages=st.session_state.messages, stream=True)
-    response = st.chat_message("assistant", avatar=":material/smart_toy:").write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "avatar": ":material/smart_toy:", "content": response})
+    st.session_state.messages.append(
+        {"role": "user", "avatar": ":material/face:", "content": prompt}
+    )
+    stream = client.chat.completions.create(
+        model="qwen-turbo", messages=st.session_state.messages, stream=True
+    )
+    response = st.chat_message(
+        "assistant", avatar=":material/smart_toy:"
+    ).write_stream(stream)
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "avatar": ":material/smart_toy:",
+            "content": response,
+        }
+    )
