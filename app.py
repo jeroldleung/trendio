0c46fcc0ca6506b80c2833bcf49232bf05292d06
jeroldleung <jeroldleung@gmail.com>
2024-10-14 23:04:58 +0800

    combine the wordcloud and academic research index analysis into one row

--- app.py
+++ app.py
@@ -42,44 +42,53 @@ def wordcloud(text):
 st.set_page_config(page_title="Trendio", layout="wide")
 st.title("Trendio")
 
-theme = st.text_input("Theme", placeholder="Search Something", label_visibility="collapsed")
+theme = st.text_input(
+    "Theme",
+    value="Artificial Intelligence",
+    placeholder="Search Something",
+    label_visibility="collapsed",
+)
 
 article_filter = CnkiService("https://search.cnki.com.cn/api/fileterresultapi/articlefileter")
 article_filter.set_params(theme=theme)
+
+paper_list = CnkiService("https://search.cnki.com.cn/api/search/listresult")
+paper_list.set_params(theme=theme)
+
+wordcloud_col, year_col = st.columns([2, 1])
+
+# get and plot the wordcloud of keywords of most downloaded articles
+paper_list.set_params(order="3")
+papers = pd.DataFrame.from_dict(paper_list.fetch()["articleList"])
+key_word = "".join(papers["keyWord"])
+wordcloud_col.pyplot(wordcloud(key_word))
 
 # get and plot the number of articles of each years
 article_filter.set_params(exclude_field="Year")
 years = pd.DataFrame.from_dict(article_filter.fetch())
 fig = px.line(years, x="FilterName", y="ArticleCount")
 fig.update_layout(title_text="Academic Research Index Analysis", yaxis_title=None, xaxis_title=None)
-st.plotly_chart(fig, use_container_width=True)
+year_col.plotly_chart(fig, use_container_width=True)
 
-col1, col2, col3 = st.columns(3)
+subject_col, type_col, level_col = st.columns(3)
 
 # get and plot the number of articles of each subjects
 article_filter.set_params(exclude_field="Subject")
 subjects = pd.DataFrame.from_dict(article_filter.fetch())
 fig = px.bar(subjects, x="ArticleCount", y="FilterName", orientation="h")
 fig.update_layout(title_text="Subject Classification", yaxis_title=None, xaxis_title=None)
-col1.plotly_chart(fig, use_container_width=True)
+subject_col.plotly_chart(fig, use_container_width=True)
 
 # get and plot the number of articles of each types
 article_filter.set_params(exclude_field="Type")
 types = pd.DataFrame.from_dict(article_filter.fetch())
 fig = px.bar(types, x="FilterName", y="ArticleCount")
 fig.update_layout(title_text="Article Type", yaxis_title=None, xaxis_title=None)
-col2.plotly_chart(fig, use_container_width=True)
+type_col.plotly_chart(fig, use_container_width=True)
 
 # get and plot the number of articles of each levels
 article_filter.set_params(exclude_field="Level")
 levels = pd.DataFrame.from_dict(article_filter.fetch())
 fig = px.bar(levels, x="ArticleCount", y="FilterName", orientation="h")
 fig.update_layout(title_text="Research Level Distribution", yaxis_title=None, xaxis_title=None)
-col3.plotly_chart(fig, use_container_width=True)
-
-paper_list = CnkiService("https://search.cnki.com.cn/api/search/listresult")
-paper_list.set_params(theme=theme)
-paper_list.set_params(order="3")
-papers = pd.DataFrame.from_dict(paper_list.fetch()["articleList"])
-key_word = "".join(papers["keyWord"])
-st.pyplot(wordcloud(key_word))
+level_col.plotly_chart(fig, use_container_width=True)
