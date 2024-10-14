5200ca679385905918a6517ebb84b1b94e50e393
jeroldleung <jeroldleung@gmail.com>
2024-10-14 09:47:37 +0800

    combine fetch and extract into fetch articles

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from wordcloud import WordCloud
 
 
-def fetch(url, theme, field=None):
+def filter_articles(url, theme, field=None):
     param = {
         "searchType": "MulityTermsSearch",
         "ParamIsNullOrEmpty": "false",
@@ -14,17 +14,13 @@ def fetch(url, theme, field=None):
         "ExcludeField": field,
     }
     response = requests.post(FILTER_URL, param)
-    return response.json()
-
-
-def extract(raw):
     res = {
         "name": [],
         "count": [],
         "code": [],
     }
-    for dic in raw:
-        name, count, code, _ = dic.values()
+    for e in response.json():
+        name, count, code, _ = e.values()
         res["name"].append(name)
         res["count"].append(count)
         res["code"].append(code)
@@ -34,7 +30,7 @@ def extract(raw):
 def wordcloud(df, word, count):
     word_freq = dict(zip(df[word], df[count]))
     wordcloud = WordCloud(
-        font_path="SimHeiBold.ttf", width=800, height=400, background_color="white"
+        font_path="SimHeiBold.ttf", width=1000, height=500, background_color="white"
     ).generate_from_frequencies(word_freq)
     fig = plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation="bilinear")
@@ -51,9 +47,9 @@ st.sidebar.header("Configuration")
 years, types, subjects = st.tabs(["Years", "Types", "Subjects"])
 
 if theme := st.sidebar.text_input("Theme", placeholder="Search something"):
-    yrs = pd.DataFrame.from_dict(extract(fetch(FILTER_URL, theme, field="Year")))
+    yrs = pd.DataFrame.from_dict(filter_articles(FILTER_URL, theme, field="Year"))
     years.line_chart(yrs, x="name", y="count", x_label="", y_label="")
-    tps = pd.DataFrame.from_dict(extract(fetch(FILTER_URL, theme, field="Type")))
+    tps = pd.DataFrame.from_dict(filter_articles(FILTER_URL, theme, field="Type"))
     types.bar_chart(tps, x="name", y="count", x_label="", y_label="", horizontal=True)
-    subjs = pd.DataFrame.from_dict(extract(fetch(FILTER_URL, theme, field="Subject")))
+    subjs = pd.DataFrame.from_dict(filter_articles(FILTER_URL, theme, field="Subject"))
     subjects.pyplot(wordcloud(subjs, "name", "count"))
